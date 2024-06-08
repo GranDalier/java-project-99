33e19c060788af6bb821c2a2c395943b7b495d17
Valentin Shabarchin <dronanval@gmail.com>
2024-06-08 23:17:17 +0400

    FIX DataInitializer.java, application.yml

--- app/src/main/java/hexlet/code/app/component/DataInitializer.java
+++ app/src/main/java/hexlet/code/app/component/DataInitializer.java
@@ -3,22 +3,24 @@ package hexlet.code.app.component;
 import hexlet.code.app.model.User;
 
 import hexlet.code.app.repository.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.ApplicationArguments;
 import org.springframework.boot.ApplicationRunner;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Component;
 
-import lombok.AllArgsConstructor;
-
 @Component
-@AllArgsConstructor
 public final class DataInitializer implements ApplicationRunner {
 
-    private final UserRepository userRepository;
-    private final PasswordEncoder passwordEncoder;
+    @Autowired
+    private UserRepository userRepository;
+
+    @Autowired
+    private PasswordEncoder passwordEncoder;
 
     @Override
     public void run(ApplicationArguments args) {
+        userRepository.flush();
         var userData = createAdmin();
         userRepository.save(userData);
     }
