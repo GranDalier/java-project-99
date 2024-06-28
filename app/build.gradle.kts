c51841580a615b262bfe5e21a73c0327514011ed
Valentin Shabarchin <dronanval@gmail.com>
2024-06-28 11:58:56 +0400

    FEAT Sentry service

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -9,6 +9,7 @@ plugins {
 	id("io.spring.dependency-management") version "1.1.5"
 	id("org.springdoc.openapi-gradle-plugin") version "1.8.0"
 	id("io.freefair.lombok") version "8.6"
+	id("io.sentry.jvm.gradle") version "4.8.0"
 }
 
 group = "hexlet.code"
@@ -16,6 +17,13 @@ version = "0.0.1-SNAPSHOT"
 
 application {
 	mainClass.set("hexlet.code.app.AppApplication")
+}
+
+sentry {
+	includeSourceContext = true
+	org = "valentin-shabarchin"
+	projectName = "java-spring-boot"
+	authToken = System.getenv("SENTRY_AUTH_TOKEN")
 }
 
 configurations {
@@ -39,6 +47,7 @@ dependencies {
 	implementation("org.springframework.boot:spring-boot-starter-security")
 	implementation("org.springframework.boot:spring-boot-starter-oauth2-resource-server")
 	implementation("org.springdoc:springdoc-openapi-starter-webmvc-ui:2.5.0")
+	implementation("io.sentry:sentry-spring-boot-starter-jakarta:7.10.0")
 
 	implementation("org.mapstruct:mapstruct:1.5.5.Final")
 	implementation("org.openapitools:jackson-databind-nullable:0.2.6")
@@ -71,3 +80,7 @@ tasks.test {
 }
 
 tasks.jacocoTestReport { reports { xml.required.set(true) } }
+
+tasks.sentryBundleSourcesJava {
+	enabled = System.getenv("SENTRY_AUTH_TOKEN") != null
+}
